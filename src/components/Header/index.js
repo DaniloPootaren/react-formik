a733250bfd7c408f66a3097fbf03dc69f36c96b6
Jean Danilo Maxwell Patrick Pootaren <danilo.pootaren@hangarww.com>
2021-09-30 14:35:00 +0400

    typo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header(props) {
   return (
     <div className="header">
-      <h1>Introdution to Formik 🚀 🤟 💪 </h1>
+      <h1>Introduction to Formik 🚀 🤟 💪 </h1>
     </div>
   );
 }
