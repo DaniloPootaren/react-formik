9daf3cb19faa13cddb36f6025624dd06f423a7ae
Jean Danilo Maxwell Patrick Pootaren <danilo.pootaren@hangarww.com>
2021-09-30 09:28:15 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h1>Introdution to Formik 🚀 🤟 💪 </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
